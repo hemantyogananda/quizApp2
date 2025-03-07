b3dd5230f4de5860524c57291a0f96f325499ebf
team ambatturvasi <teamambatturvasi@gmail.com>
2025-03-07 07:31:39 +0530

    EAS develop build completed

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your applic!</Text>
+      <Text>Open up App.tsx to start working on your applications!</Text>
       <StatusBar style="auto" />
     </View>
   );
