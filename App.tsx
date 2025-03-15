4c143f4dba05ba99897d7ea07bff259f945f975e
team ambatturvasi <teamambatturvasi@gmail.com>
2025-03-15 08:18:51 +0530

    Added app config ts

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New Text Updates</Text>
+      <Text>New Text Updates updated 2</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app.config.ts
@@ -0,0 +1,30 @@
+// /* eslint-env node */
+import { ExpoConfig } from '@expo/config-types';
+import { coerce } from 'semver';
+import pkg from './package.json';
+
+const currentVersion = coerce(pkg.version);
+const androidVersion = currentVersion.major * 10000 + currentVersion.minor * 100 + currentVersion.patch;
+const expoVersion = pkg.version;
+
+const config: ExpoConfig = {
+
+  name: 'quizapp',
+  slug: 'quizapp',
+  extra: {
+    dbName: 'test',
+    eas: {
+        projectId: "8ea15a44-06e8-41d0-bab5-a950598ff1a1"
+    },
+  },
+android: {
+    package: 'com.amarjanica.quizapp',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    versionCode: androidVersion,
+  },
+};
+
+export default config;
