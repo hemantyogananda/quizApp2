328a73f2c033ac6efe712a484aa73cedbbb3ce95
team ambatturvasi <teamambatturvasi@gmail.com>
2025-03-15 11:17:38 +0530

    Testing text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>New Text Updates updated 2</Text>
+      <Text>New Text Updates updated</Text>
       <StatusBar style="auto" />
     </View>
   );
